e72b3a36874c75dc2296d457c4a7f5ccffac30b7
Ryan Smit <ryansmit82@gmail.com>
2022-03-20 00:27:25 -0600

    major update, filled in AI turn

--- UserInterface.java
+++ UserInterface.java
@@ -3,27 +3,33 @@ import java.util.Scanner;
 Ryan Smit
 
 a Class designed to display the menu, asking where the user wants to put their symbol (1-9)
-////returns either 'r' or 'p' to the Driver (GameOfLife)/////
+
 checks that input is is valid type and number
 
-Oct 30 2021- created
+Oct 30 2021 - created
+Mar 19 2022 - comments and debug mode displays
 */
 
 
 public class UserInterface{
 
-    private boolean done;
+    private boolean done;                                   // done = true if a correct input is received
+    private final int FIRST = 0;
 
     public UserInterface(){
     	done = false;
     }
 
+    /* for player to choose symbol
+    ** returns players choice 
+    */
     public char chooseSymbol(){
-        final int FIRST = 0;
+        done = false;
+        char selection = ' ';
+        char type = ' ';
 	    Scanner in = new Scanner(System.in);
-	    String line;
-	    char selection;
-	    char type = ' ';
+        String line;
+	    
 
     	while (done == false)
     	{
@@ -36,9 +42,11 @@ public class UserInterface{
 		return(type);
     }
 
+    /* processes player input as parameter
+    ** acceptable inputs are o, O, x, X, d, D
+    */ 
     public char processChoice(char selection){
-    	char type = ' ';
-
+        char type = ' ';
 		switch(selection){
 			case 'x':
 			case 'X':
@@ -54,9 +62,16 @@ public class UserInterface{
 
 			case 'd':
 			case 'D':
-				GameStatus.debugModeOn = true;
-				System.out.println("Debug mode on");
-				break;
+				if (GameStatus.debugModeOn == false){
+                    GameStatus.debugModeOn = true;
+                    System.out.println("Debug mode on");
+				    break;
+                }
+				else if (GameStatus.debugModeOn == true){
+                    GameStatus.debugModeOn = false;
+                    System.out.println("Debug mode off");
+				    break;
+                }
 
 			default:
 				System.out.println("Please enter 'X' or 'O'");
@@ -64,83 +79,95 @@ public class UserInterface{
 		return(type);
     }
 
-    public int start(char symbol){
+    /* starts players turn, takes player's symbol
+    ** returns where the player wants to place symbol
+    */
+    public char start(char symbol){                                             
         done = false;
-		final int FIRST = 0;
+        char selection = ' ';
+        char type = ' ';
 	    Scanner in = new Scanner(System.in);
-	    String line;
-	    char selection;
-	    int type = 0;
+        String line;
 
     	while (done == false)
     	{
-    		displayMenu(symbol);
+    		System.out.format("Choose where to place your %c: \n", symbol);
     		line = in.nextLine();
     		selection = line.charAt(FIRST);
     		type = processMenu(selection);
     	}
     	in.close();
+        if (GameStatus.debugModeOn == true){
+            System.out.format("player chose %c\n", type);
+        }
+
 		return(type);
     }
 
-    public void displayMenu(char symbol){
-        System.out.format("Choose where to place your %c: \n", symbol);
-    }
 
-    public int processMenu(char selection){
-    	int type = 0;
-
+    /* processes player input as parameter
+    ** acceptable inputs are 1,2,3,4,5,6,7,8,9, d, D
+    */ 
+    public char processMenu(char selection){
+        char type = ' ';
 		switch(selection){
 			case '1':
-				type = 1;
+				type = '1';
 				done = true;
 				break;
 
             case '2':
-				type = 2;
+				type = '2';
 				done = true;
 				break;
 
             case '3':
-				type = 3;
+				type = '3';
 				done = true;
 				break;
 
             case '4':
-				type = 4;
+				type = '4';
 				done = true;
 				break;
 
             case '5':
-				type = 5;
+				type = '5';
 				done = true;
 				break;
 
             case '6':
-				type = 6;
+				type = '6';
 				done = true;
 				break;
 
             case '7':
-				type = 7;
+				type = '7';
 				done = true;
 				break;
 
             case '8':
-				type = 8;
+				type = '8';
 				done = true;
 				break;
 
             case '9':
-				type = 9;
+				type = '9';
 				done = true;
 				break;
 
 			case 'd':
 			case 'D':
-				GameStatus.debugModeOn = true;
-				System.out.println("Debug mode on");
-				break;
+                if (GameStatus.debugModeOn == false){
+                    GameStatus.debugModeOn = true;
+                    System.out.println("Debug mode on");
+                    break;
+                }
+                else if (GameStatus.debugModeOn == true){
+                    GameStatus.debugModeOn = false;
+                    System.out.println("Debug mode off");
+                    break;
+                }
 
 			default:
 				System.out.println("Please enter a number between 1 and 9");
@@ -148,4 +175,67 @@ public class UserInterface{
 		return(type);
     }
 
+    /* asks player to chose heads or tails
+    ** returns 0 (heads) or 1 (tails)
+    */
+    public int flipCoin(){
+        char selection = ' ';
+        int coin = -1;
+        done = false;
+	    Scanner in = new Scanner(System.in);
+        String line;
+
+        System.out.println("Flip a coin to see who goes first");
+    	while (done == false)
+    	{
+    		System.out.println("Choose (h)eads or (t)ails: ");
+    		line = in.nextLine();
+    		selection = line.charAt(FIRST);
+    		coin = processFlip(selection);
+    	}
+    	in.close();
+        if (GameStatus.debugModeOn == true){
+            System.out.format("player chose %d\n", coin);
+        }
+
+		return(coin);
+    }
+
+    /* processes player input as parameter
+    ** acceptable inputs are h, H, t, T, d, D
+    ** heads = 0, tails = 1
+    */ 
+    public int processFlip(char selection){
+        int coin = -1;
+		switch(selection){
+			case 'h':
+			case 'H':
+				coin = 0;
+				done = true;
+				break;
+
+			case 't':
+			case 'T':
+				coin = 1;
+				done = true;
+				break;
+
+			case 'd':
+			case 'D':
+				if (GameStatus.debugModeOn == false){
+                    GameStatus.debugModeOn = true;
+                    System.out.println("Debug mode on");
+				    break;
+                }
+				else if (GameStatus.debugModeOn == true){
+                    GameStatus.debugModeOn = false;
+                    System.out.println("Debug mode off");
+				    break;
+                }
+
+			default:
+				System.out.println("Please enter 'X' or 'O'");
+		}
+		return(coin);
+    }
 }
--- World.java
+++ World.java
@@ -1,32 +1,31 @@
 import java.util.Scanner;
+import java.util.Random;
 /*
 Ryan Smit
 
-creates and stores the Critter[][] of simulated creatures
-each round, displays the current [][], the projected [][] after births/deaths and the [][] after the Taminator operations
-continues until the user quits
-identifies how many neighbours a Critter has in order to...
-handles the "regular" rules for births/deaths
-carries out the functions of the Taminator, "kills" its neighbours before it randomly moves, checks that the move is valid
 
-assumes all Taminators have appearance 'T' and Critters are either '*' or ' '
 
 Oct 30 2021 - created
+Mar 19 2022 - comments and
 */
 
 
 public class World{
-    public static final int ROWS = 3;
-    public static final int COLUMNS = 3;
-    public static final String HORIZONTAL_LINE = " _".repeat(COLUMNS);
-    public char [][] current;
-    private Player aPlayer = new Player();
-    private UserInterface anInterface = new UserInterface();
-    public boolean done = false;
-    public boolean winner = false;
+    private static final int ROWS = 3;
+    private static final int COLUMNS = 3;
+    private static final String HORIZONTAL_LINE = " _".repeat(COLUMNS);
+    private char [][] current;                                              // the "map"
+    private char aPlayer = ' ';                                             // default player = ' '
+    private char computerPlayer = 'O';                                      // default AI = 'O'
+    private UserInterface anInterface;
+    private int coin;                        
+    private boolean winner = false;                                         // winner = true if player in winner
+    private boolean stalemate = false;
 
     public World(){
         current = new char[ROWS][COLUMNS];
+        anInterface = new UserInterface();
+        // initializes char's in map to '1' to '9'
         int count = 1;
         for (int r = 0; r < ROWS; r++){
             for (int c = 0; c < COLUMNS; c++){
@@ -34,10 +33,14 @@ public class World{
                 count ++;
             }
         }
-        aPlayer.setAppearance(anInterface.chooseSymbol());
+        //player chooses symbol
+        aPlayer = anInterface.chooseSymbol();
+        if (aPlayer == 'O')
+            computerPlayer = 'X';
+        coin = anInterface.flipCoin();
     }
 
-
+    // prints out the current "map"
     public void display(){ 
         for (int r = 0; r < ROWS; r++){
             System.out.println(HORIZONTAL_LINE);
@@ -50,35 +53,94 @@ public class World{
     } 
 
 
-    public char [][] getCurrent(){
-        return(current);
-    }
+    /* runs one set of player and AI turns each loop
+    ** ends when either player or AI gets 3 in a row (checkDone())
+    */
+    public void runTurn(){                                                  //debugModeOn 
+        char entry = ' ';
+        boolean valid;
+        Random rand = new Random();
+        int flip = rand.nextInt(2);
+        char coinFlipped = ' ';
+        if (flip == 0)
+            coinFlipped = 'H';
+        else if (flip == 1)
+            coinFlipped = 'T';
 
-    
-    public void runTurn(){                            //debugModeOn
-        while (done == false){
+        if (flip == coin){ //player goes first
+            System.out.format("Coin flip was %c, you go first!\n", coinFlipped);
             display();
-            boolean valid = false;
-            while (valid == false){
-                int entry = anInterface.start(aPlayer.getAppearance());
-                valid = checkEmpty(entry);
-                if (valid == false)
-                    System.out.println("Please choose an empty space");
+            while (true){   
+                valid = false;
+                while (valid == false){
+                    entry = anInterface.start(aPlayer);
+                    valid = checkEmpty(entry);
+                    if (valid == false)
+                        System.out.println("Please choose an empty space");
+                }
+                placeSymbol(aPlayer, entry);
+                System.out.format("You successfully placed your %c at position %c\n", aPlayer, entry);
+                display();
+                if (checkDone(aPlayer)){
+                    winner = true;
+                    break;
+                }     
+                isStalemate();    
+                if (stalemate == true)
+                    break;   
+                computerTurn();
+                display();
+                if (checkDone(computerPlayer))
+                    break;
+                isStalemate();    
+                if (stalemate == true)
+                    break;  
             }
-            //change entry to aPlayer.getAppearance()
-            checkDone();
-            if (done == true)
-                break;
-            //do AI's turn
-            checkDone();
         }
-
-        if (winner = true)
-            System.out.println("Congratulations! You won!");
-        else
-            System.out.println("You lost...");
+        else { // AI goes first
+            System.out.format("Coin flip was %c...\n", coinFlipped);
+            display();
+            while (true){
+                computerTurn();
+                display();
+                if (checkDone(computerPlayer))
+                    break;       
+                isStalemate();    
+                if (stalemate == true)
+                    break;  
+                valid = false;
+                while (valid == false){
+                    entry = anInterface.start(aPlayer);
+                    valid = checkEmpty(entry);
+                    if (valid == false)
+                        System.out.println("Please choose an empty space");
+                }
+                placeSymbol(aPlayer, entry);
+                System.out.format("You successfully placed your %c at position %c\n", aPlayer, entry);
+                display();
+                if (checkDone(aPlayer)){
+                    winner = true;
+                    break;
+                } 
+                isStalemate();    
+                if (stalemate == true)
+                    break;  
+            }
+        }
+        if (stalemate == true)
+            System.out.println("No more moves left, no one wins...");
+        else {
+            if (winner == true)
+                System.out.println("Congratulations! You won!");
+            else
+                System.out.println("You lost...");
+        }     
     }
 
+    /* checks if player's choice is an empty square
+    ** returns true if empty
+    ** assumes default square numbers means empty
+    */
     public boolean checkEmpty(int position){
         boolean result = false;
 
@@ -134,8 +196,324 @@ public class World{
         return(result);
     }
 
-    public void checkDone(){
-
+    /* checks if passed char has 3 in a row
+    **
+    */
+    public boolean checkDone(char testChar){                                       // debug mode
+        boolean done = false;
+        if (current[1][1] == testChar){
+            if (current[1][0] == testChar && current[1][2] == testChar)
+                return done = true;
+            else if (current[0][1] == testChar && current[2][1] == testChar)
+                return done = true;
+            else if (current[0][0] == testChar && current[2][2] == testChar)
+                return done = true;
+            else if (current[0][2] == testChar && current[2][0] == testChar)
+                return done = true;
+        }
+        if (current[0][0] == testChar){
+            if (current[0][1] == testChar && current[0][2] == testChar)
+                return done = true;
+            else if (current[1][0] == testChar && current[2][0] == testChar)
+                return done = true;
+        }
+        if (current[2][2] == testChar) {
+            if (current[2][0] == testChar && current[2][1] == testChar)
+                return done = true;
+            else if (current[0][2] == testChar && current[1][2] == testChar)
+                return done = true;
+        }
+        return done;
     }
     
+    /* sets symbol at chosen square 
+    ** already checked if empty square
+    */
+    public void placeSymbol(char symbol, char position){
+        switch(position){
+			case '1':
+                current[0][0] = symbol;
+				break;
+
+            case '2':
+                current[0][1] = symbol;
+				break;
+
+            case '3':
+                current[0][2] = symbol;
+				break;
+
+            case '4':
+                current[1][0] = symbol;
+				break;
+
+            case '5':
+                current[1][1] = symbol;
+				break;
+
+            case '6':
+                current[1][2] = symbol;
+				break;
+
+            case '7':
+                current[2][0] = symbol;
+				break;
+
+            case '8':
+                current[2][1] = symbol;
+				break;
+
+            case '9':
+                current[2][2] = symbol;
+				break;
+
+            default:
+                break;
+        }
+    }
+
+    /* logic for AI's turn
+    ** 1. win if about to win
+    ** 2. block player if player about to win
+    ** 3. choose best place
+    **      a. middle square, 5
+    **      b. corner square, 1, 3, 7, 9
+    **      c. 2, 4, 6, 8
+    */
+    public void computerTurn(){                                                     //debugmodeon
+        char close = checkIfClose(computerPlayer);
+        if (close != '0'){ // AI about to win
+            placeSymbol(computerPlayer, close);
+        }
+        else {
+            close = checkIfClose(aPlayer);
+            if (close != '0'){ //player about to win
+                placeSymbol(computerPlayer, close);
+            }
+            else { //choose a spot
+                if (checkEmpty(5)){ //middle is empty
+                    placeSymbol(computerPlayer, '5');
+                    close = '5';
+                }    
+                else if (checkSymbol(5)){ //computer has middle
+                    if (checkEmpty(1)){ 
+                        placeSymbol(computerPlayer, '1');
+                        close = '1';
+                    } 
+                    else if (checkEmpty(3)){ 
+                        placeSymbol(computerPlayer, '3');
+                        close = '3';
+                    } 
+                    else if (checkEmpty(7)){ 
+                        placeSymbol(computerPlayer, '7');
+                        close = '7';
+                    } 
+                    else if (checkEmpty(9)){ 
+                        placeSymbol(computerPlayer, '9');
+                        close = '9';
+                    } 
+                    else if (checkEmpty(2)){
+                        placeSymbol(computerPlayer, '2');
+                        close = '2';
+                    } 
+                    else if (checkEmpty(4)){ 
+                        placeSymbol(computerPlayer, '4');
+                        close = '4';
+                    } 
+                    else if (checkEmpty(6)){ 
+                        placeSymbol(computerPlayer, '6');
+                        close = '6';
+                    } 
+                    else if (checkEmpty(8)){ 
+                        placeSymbol(computerPlayer, '8');
+                        close = '8';
+                    } 
+                }
+                else {  //player has middle square
+                    if (checkEmpty(1)){ 
+                        placeSymbol(computerPlayer, '1');
+                        close = '1';
+                    } 
+                    else if (checkEmpty(3)){ 
+                        placeSymbol(computerPlayer, '3');
+                        close = '3';
+                    } 
+                    else if (checkEmpty(7)){ 
+                        placeSymbol(computerPlayer, '7');
+                        close = '7';
+                    } 
+                    else if (checkEmpty(9)){ 
+                        placeSymbol(computerPlayer, '9');
+                        close = '9';
+                    } 
+                    else if (checkEmpty(2)){
+                        placeSymbol(computerPlayer, '2');
+                        close = '2';
+                    } 
+                    else if (checkEmpty(4)){ 
+                        placeSymbol(computerPlayer, '4');
+                        close = '4';
+                    } 
+                    else if (checkEmpty(6)){ 
+                        placeSymbol(computerPlayer, '6');
+                        close = '6';
+                    } 
+                    else if (checkEmpty(8)){ 
+                        placeSymbol(computerPlayer, '8');
+                        close = '8';
+                    } 
+                }
+            }  
+        } 
+        if (close == '0'){
+            System.out.println("Error, map is full?!");
+            System.exit(0);
+        }
+        System.out.format("%c placed at position %c", computerPlayer, close);
+    }
+
+    /*
+    **
+    */
+    public boolean checkSymbol(int position){ //returns true if computer has the spot
+        boolean result = false;
+
+        switch(position){
+			case 1:
+                if (current[0][0] == computerPlayer)
+                    result = true;
+				break;
+
+            case 2:
+                if (current[0][1] == computerPlayer)
+                    result = true;
+				break;
+
+            case 3:
+                if (current[0][2] == computerPlayer)
+                    result = true;
+				break;
+
+            case 4:
+                if (current[1][0] == computerPlayer)
+                    result = true;
+				break;
+
+            case 5:
+                if (current[1][1] == computerPlayer)
+                    result = true;
+				break;
+
+            case 6:
+                if (current[1][2] == computerPlayer)
+                    result = true;
+				break;
+
+            case 7:
+                if (current[2][0] == computerPlayer)
+                    result = true;
+				break;
+
+            case 8:
+                if (current[2][1] == computerPlayer)
+                    result = true;
+				break;
+
+            case 9:
+                if (current[2][2] == computerPlayer)
+                    result = true;
+				break;
+
+            default:
+                break;
+        }
+        return(result);
+    }
+
+    /* checks if someone (parameter) close to winning
+    ** returns the (first found) empty space needed to win/block
+    */
+    public char checkIfClose(char testChar){                                       // debug mode
+        char next = '0'; 
+        if (current[0][0] == testChar){
+            if (current[0][1] == testChar)
+                return next = '3';
+            if (current[0][2] == testChar)
+                return next = '2';
+            if (current[1][0] == testChar)
+                return next = '7';
+            if (current[1][1] == testChar)
+                return next = '9';
+            if (current[2][0] == testChar)
+                return next = '4';
+            if (current[2][2] == testChar)
+                return next = '5';
+        }
+        if (current[0][1] == testChar){
+            if (current[0][2] == testChar)
+                return next = '1';
+            if (current[1][1] == testChar)
+                return next = '8';
+            if (current[2][1] == testChar)
+                return next = '5';
+        }
+        if (current[0][2] == testChar) {
+            if (current[1][1] == testChar)
+                return next = '7';
+            if (current[1][2] == testChar)
+                return next = '9';
+            if (current[2][0] == testChar)
+                return next = '5';
+            if (current[2][2] == testChar)
+                return next = '6';                
+        }
+        if (current[1][0] == testChar){
+            if (current[1][1] == testChar)
+                return next = '6';
+            if (current[1][2] == testChar)
+                return next = '5';
+            if (current[2][0] == testChar)
+                return next = '1';
+        }
+        if (current[1][1] == testChar){
+            if (current[1][2] == testChar)
+                return next = '4';
+            if (current[2][0] == testChar)
+                return next = '3';
+            if (current[2][1] == testChar)
+                return next = '2';
+            if (current[2][2] == testChar)
+                return next = '1';    
+        }
+        if (current[2][1] == testChar){
+            if (current[2][0] == testChar)
+                return next = '9';
+            if (current[2][2] == testChar)
+                return next = '7';
+        }
+        if (current[2][2] == testChar){
+            if (current[2][0] == testChar)
+                return next = '8';
+            if (current[1][2] == testChar)
+                return next = '3';
+        }
+        return next;
+    }
+    
+    /* checks if there are no moves left
+    ** changes this.stalemate to true and if there are empty spaces, changes it back to false
+    */
+    public void isStalemate(){                                       // debug mode
+        stalemate = true;
+        for (int r = 0; r < ROWS; r++){
+            for (int c = 0; c < COLUMNS; c++){
+                if (current[r][c] != 'O' && current[r][c] != 'X'){
+                    stalemate = false;
+                    return;
+                }
+            }
+
+        }
+    }
+
 }
